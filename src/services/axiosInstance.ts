01026539915f31975ca278a9f2922859feaa61a3
Jhon Salguero <svjchrysler@gmail.com>
2023-01-29 23:59:38 -0400

    Axios has been set up

---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import {API_URL} from "../helpers/Constants";
+
+const instance = axios.create({
+  baseURL: API_URL,
+  timeout: 30000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
+  withCredentials: false,
+});
+
+instance.interceptors.response.use(
+  response => Promise.resolve(response.data),
+  error => {
+    if ([401, 403].includes(error?.response?.status)) {
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
