a369ba47d625d493e50b3177a84b23492f419556
Jhon Salguero <svjchrysler@gmail.com>
2023-01-29 23:58:06 -0400

    new colors added

---
+++ src/styles/COLORS.ts
@@ -0,0 +1,13 @@
+
+const WHITE: string = '#FFFFFF';
+const BLACK: string = '#020202';
+const GRAY: string = '#9B9898';
+const DARK_BLACK: string = '#000000';
+
+
+export default {
+  WHITE,
+  BLACK,
+  GRAY,
+  DARK_BLACK,
+}
